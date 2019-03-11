71984802a034125a28622cc53564f1ae2866685b
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-03-12 00:26:44 +0100

    [BROKEN] New hexes, directions are bricked

--- static/js/hex.js
+++ static/js/hex.js
@@ -15,13 +15,22 @@ class Hex {
 
     create() {
         let hex = $('<div>')
-        hex.addClass('hex')
+        let hex0 = $('<div>')
+        let hex1 = $('<div>')
+        //hex.addClass('hex')
+        hex.addClass('testHex')
+        hex0.addClass('testHex')
+        hex1.addClass('testHex')
         hex.css('left', this.z * 116 - 29 * this.z)
         if (this.z % 2 == 0) {
             hex.css('top', this.x * 100)
         } else {
             hex.css('top', this.x * 100 + 50)
         }
+        hex0.css('transform', 'rotate(60deg)')
+        hex1.css('transform', 'rotate(120deg)')
+        hex.append(hex0)
+        hex.append(hex1)
         hex[0].hex = this
         this.dirOut = -1
         this.object = hex
