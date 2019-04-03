642bd95e79a6501b6d182ea705cee2c4df1ad19f
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2019-04-03 12:39:06 +0400

    Removed unused declarations

--- static/js_game/player.js
+++ static/js_game/player.js
@@ -3,19 +3,13 @@ class Player {
         this.container = new THREE.Object3D()
         this.model = new PlayerModel()
 
-        let geo = new THREE.BoxGeometry(20, 20, 20)
-        let mat = new THREE.MeshBasicMaterial({
-            color: 0x0000ff,
-            wireframe: true
-        })
-
         let klasa = this
 
         this.model.loadModel(function (modeldata) {
             klasa.player = modeldata
             klasa.container.add(klasa.player)
 
-            klasa.axes = new THREE.AxesHelper(50)
+            klasa.axes = new THREE.AxesHelper(20)
             klasa.player.add(klasa.axes)
             klasa.axes.position.y = 10
         })
